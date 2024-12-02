83a1b02a0a607901e80827fc4cf4756514efcac4
Anh5the <lehoanganh2142003@gmail.com>
2024-12-02 12:09:10 +0700

    Done Practice Lesson Page

---
+++ components/modals/hearts-modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useHeartsModal } from "@/store/use-hearts-modal";
+
+export const HeartsModal = () => {
+  const router = useRouter();
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = useHeartsModal();
+
+  useEffect(() => setIsClient(true), []);
+  if (!isClient) {
+    return null;
+  }
+
+  const onClick = () => {
+    close();
+    router.push("/store");
+  };
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center w-full justify-center">
+            <Image src="/bad.png " alt="bad" height={130} width={130} />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            You ran out of hearts
+          </DialogTitle>
+          <DialogDescription className="text-center text-base">
+            Buy more hearts in the store to continue learning courses{" "}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-2">
+          <div className="flex flex-col gap-y-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={onClick}
+            >
+              Buy now at shop
+            </Button>
+            <Button
+              variant="primaryOutline"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              No thanks
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
---
+++ components/modals/practice-modal.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { usePracticeModal } from "@/store/use-practice-modal";
+
+export const PracticeModal = () => {
+  const [isClient, setIsClient] = useState(false);
+  const { isOpen, close } = usePracticeModal();
+
+  useEffect(() => setIsClient(true), []);
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={close}>
+      <DialogContent className="max-w-md">
+        <DialogHeader>
+          <div className="flex items-center w-full justify-center">
+            <Image src="/heart.png " alt="heart" height={120} width={120} />
+          </div>
+          <DialogTitle className="text-center font-bold text-2xl">
+            Practice with the lesson{" "}
+          </DialogTitle>
+          <DialogDescription className="text-center text-base">
+            Practicing lessons helps you regain hearts and points, and you
+            cannot lose them during practice.{" "}
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="mb-2">
+          <div className="flex flex-col gap-y-4 w-full">
+            <Button
+              variant="primary"
+              className="w-full"
+              size="lg"
+              onClick={close}
+            >
+              I understand
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
