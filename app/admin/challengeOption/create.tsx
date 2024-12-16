2843b145473a81a03fb416766522ca5860568053
Anh5the <lehoanganh2142003@gmail.com>
2024-12-16 13:28:12 +0700

    done project

---
+++ app/admin/challengeOption/create.tsx
@@ -0,0 +1,25 @@
+import {
+  BooleanInput,
+  Create,
+  NumberInput,
+  ReferenceInput,
+  required,
+  SelectInput,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
+
+export const challengeOptionCreate = () => {
+  return (
+    <Create>
+      <SimpleForm>
+        <TextInput source="text" validate={[required()]} label="Text" />
+
+        <BooleanInput source="correct" label="Correct option" />
+        <ReferenceInput source="challengeId" reference="challenges" />
+        <TextInput source="imageSrc" label="Image URL" />
+        <TextInput source="audioSrc" label="Audio URL" />
+      </SimpleForm>
+    </Create>
+  );
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin", // Sửa lỗi chính tả
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS", // Thêm dấu phẩy giữa PUT và DELETE
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+          {
+            key: "Content-Range",
+            value: "bytes 0-9/*", // Sửa định dạng
+          },
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true", // Tùy chọn thêm nếu dùng cookie
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
